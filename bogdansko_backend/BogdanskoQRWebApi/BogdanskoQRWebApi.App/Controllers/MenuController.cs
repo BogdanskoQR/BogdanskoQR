54ba3e597803ede4061cd206cbe282bc97cab88d
Antonio <157141622+BogdanskoQR@users.noreply.github.com>
2024-01-19 17:45:32 +0100

    backend added

---
+++ bogdansko_backend/BogdanskoQRWebApi/BogdanskoQRWebApi.App/Controllers/MenuController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BogdanskoQRWebApi.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+
+    }
+}
