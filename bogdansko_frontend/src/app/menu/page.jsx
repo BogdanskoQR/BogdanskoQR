fb0a3f7dbb5e187d7e44f8f2caf460441c2e1593
Antonio Pavic <157141622+BogdanskoQR@users.noreply.github.com>
2024-01-21 22:58:33 +0100

    added menu for mobile

--- bogdansko_frontend/src/app/menu/page.jsx
+++ bogdansko_frontend/src/app/menu/page.jsx
@@ -1,3 +1,22 @@
+import "./MenuPage.css";
+import drinksData from "../../data/drinksData";
 export default function Page() {
-    return <h1>Hello, this is dashboard</h1>
-  }
+  return (
+    <div className="menuPageWrapper">
+      <div className="heading">
+        <h2>Maxim</h2>
+        <h3>Coffee Bar</h3>
+      </div>
+      <div className="categoriesWrapper">
+        {drinksData.map((oneCategorie) => (
+          <div className="oneCategorieCart">
+            <div className="categorieTitle">
+              <h3>{oneCategorie.name}</h3>
+            </div>
+            <img src={oneCategorie.img} alt="img" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
